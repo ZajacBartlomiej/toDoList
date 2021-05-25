430e3ab3fd076138efaca8d59dc9362577ad74ac
ZajacBartlomiej <zajac.bartlomiej28@gmail.com>
2021-05-25 20:46:47 +0200

    create a plugin

---
+++ bz-todo/bz-todo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: ToDo List Bartlomiej Zajac
+ * Plugin URI: http://bartholomewzajac.pl/todo-list-plugin/
+ * Description: This plugin create a todo list on your website.
+ * Version: 1.0
+ * Author: Bartlomiej Zajac
+ * Author URI: http://bartholomewzajac.pl/
+ */
+
+ echo 'hello world';
