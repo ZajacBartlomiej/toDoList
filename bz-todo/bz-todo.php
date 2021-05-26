9917ab3e92db3f9237f4b4d07850e44e6f427c35
ZajacBartlomiej <zajac.bartlomiej28@gmail.com>
2021-05-26 08:04:25 +0200

    create CPT and enqueue styles

--- bz-todo/bz-todo.php
+++ bz-todo/bz-todo.php
@@ -8,4 +8,57 @@
  * Author URI: http://bartholomewzajac.pl/
  */
 
- echo 'hello world';
+//  if ( !defined('ABSPATH') )
+//  {
+//      echo 'what?';
+//      exit;
+//  }
+
+ class BzToDo {
+
+    public function __construct()
+    {
+        // Add new CPT
+        add_action('init', array($this, 'create_custom_post_type'));
+
+        // Add styles and scipts
+        add_action('wp_enqueue_scripts', array($this, 'load_assets'));
+    }
+
+    public function create_custom_post_type()
+    {
+        
+        $args = array(
+
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array('title'),
+            'taxonomies' => array('topics', 'category' ),
+            'exclude_from_search' => true,
+            'publicly_queryable' => false,
+            'capability' => 'manage_options',
+            'labels' => array(
+                'name' => 'ToDo List',
+                'singular_name' => 'Task'
+            ),
+            'menu_icon' => 'dashicons-editor-ol',
+        );
+
+        register_post_type('ToDo List', $args);
+
+    }
+
+    public function load_assets()
+    {
+        wp_enqueue_style(
+            'bz-todo',
+            plugin_dir_url( __FILE__ ) . 'css/bz-todo.css',
+            array(),
+            1,
+            'all'
+        );
+    }
+
+ }
+
+ new BzToDO;
