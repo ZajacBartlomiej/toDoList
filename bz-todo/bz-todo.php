3d4d33730c183753e4d731cac8043c0b2be060d1
ZajacBartlomiej <zajac.bartlomiej28@gmail.com>
2021-05-30 21:46:55 +0200

    Update bz-todo.php

--- bz-todo/bz-todo.php
+++ bz-todo/bz-todo.php
@@ -8,48 +8,13 @@
  * Author URI: http://bartholomewzajac.pl/
  */
 
-//  if ( !defined('ABSPATH') )
-//  {
-//      echo 'what?';
-//      exit;
-//  }
-
  class BzToDo {
 
     public function __construct()
     {
-        // Add new CPT
-        // add_action('init', array($this, 'create_custom_post_type'));
-
         // Add styles and scipts
         add_action('wp_enqueue_scripts', array($this, 'load_assets'));
-
-        // Load code
-        // add_action('bz-todo', array($this, 'load_code'));
     }
-
-    // public function create_custom_post_type()
-    // {
-        
-    //     $args = array(
-
-    //         'public' => true,
-    //         'has_archive' => true,
-    //         'supports' => array('title'),
-    //         'taxonomies' => array('topics', 'category' ),
-    //         'exclude_from_search' => true,
-    //         'publicly_queryable' => false,
-    //         'capability' => 'manage_options',
-    //         'labels' => array(
-    //             'name' => 'ToDo List',
-    //             'singular_name' => 'Task'
-    //         ),
-    //         'menu_icon' => 'dashicons-editor-ol',
-    //     );
-
-    //     register_post_type('ToDo List', $args);
-
-    // }
 
     public function load_assets()
     {
@@ -69,11 +34,6 @@
             true
         );
     }
-
-    // public function load_code()
-    // {
-    //     return 'works';
-    // }
 
  }
 
